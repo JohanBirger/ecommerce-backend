84e1d0ecf4b300b0ca61fe13457788fa4471bc72
JohanBirger <johankoddar@gmail.com>
2023-07-10 01:45:46 +0200

    fresh nest environment

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // 1.1 Import the mongoose module
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module'; // 2.1 Import the product module
+import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
+import 'dotenv/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://johbirger:${process.env.MONGOKEY}@cluster0.ddwvlsd.mongodb.net/`), // 1.2 Setup the database
+    //ConfigModule.forRoot(), //for gcloud
+    ProductModule, UserModule, AuthModule, CartModule, // 2.2 Add the product module
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
