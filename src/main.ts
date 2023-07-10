e8decf0881f9290bf3dc72c404637c26e487ce10
JohanBirger <johankoddar@gmail.com>
2023-07-10 13:19:06 +0200

    edit env

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { connectDB } from './database'; // Import the function to establish the database connection
+import 'dotenv/config';
 
 async function bootstrap() {
   await connectDB(); // Establish the database connection
@@ -9,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'https://ecommerce-gules-two.vercel.app', // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL, // Replace with your frontend URL 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 200,
