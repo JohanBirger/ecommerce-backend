3f966f4f96857fdbd588aa1ef07c1f9fa1d72005
JohanBirger <johankoddar@gmail.com>
2023-07-25 13:37:39 +0200

    added safeguard not to allow actual payment

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -4,13 +4,19 @@ import { CheckoutController } from './checkout.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CheckoutSchema } from './schema/checkout.schema';
 import { CartSchema } from 'src/cart/schemas/cart.schema';
+import { SendgridService } from '../utils/sendgrid.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
+import { CryptoService } from 'src/utils/crypto/crypto.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forFeature([{ name: 'Checkout', schema: CheckoutSchema }]),
     MongooseModule.forFeature([{ name: 'User', schema: CartSchema }]),
+    
   ],
-  providers: [CheckoutService],
+  providers: [CheckoutService,SendgridService,UserService,CryptoService],
   controllers: [CheckoutController]
 })
 export class CheckoutModule {}
